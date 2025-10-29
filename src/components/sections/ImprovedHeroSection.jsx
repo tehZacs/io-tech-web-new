60ccf0ba2d645a67b2162cbbdc0ea0425f747324
Antonio Rodríguez <rodriguez.antonio@gmail.com>
2025-10-29 16:57:01 -0600

    Invertir notch SVG: ahora apunta hacia abajo correctamente

--- src/components/sections/ImprovedHeroSection.jsx
+++ src/components/sections/ImprovedHeroSection.jsx
@@ -206,14 +206,14 @@ export const ImprovedHeroSection = () => {
         preserveAspectRatio="none"
         xmlns="http://www.w3.org/2000/svg"
       >
-        {/* Notch shape filled with section color */}
+        {/* Notch shape filled with section color - inverted */}
         <path
-          d="M0,0 L300,0 Q320,0 330,15 L380,75 Q400,95 420,95 L780,95 Q800,95 820,75 L870,15 Q880,0 900,0 L1200,0 L1200,100 L0,100 Z"
+          d="M0,100 L300,100 Q320,100 330,85 L380,25 Q400,5 420,5 L780,5 Q800,5 820,25 L870,85 Q880,100 900,100 L1200,100 L1200,0 L0,0 Z"
           fill="rgb(3, 7, 18)"
         />
         {/* Accent line following the notch edge */}
         <path
-          d="M330,15 L380,75 Q400,95 420,95 L780,95 Q800,95 820,75 L870,15"
+          d="M330,85 L380,25 Q400,5 420,5 L780,5 Q800,5 820,25 L870,85"
           fill="none"
           className="stroke-accent-400"
           strokeWidth="2"
