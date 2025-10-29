b03f2e9112818d7ce0f5ef6af2ec654bed225193
Antonio Rodríguez <rodriguez.antonio@gmail.com>
2025-10-29 17:06:17 -0600

    Separar en dos paths simples: solo lados rellenos, centro (notch) transparente

--- src/components/sections/ServicesSection.jsx
+++ src/components/sections/ServicesSection.jsx
@@ -89,16 +89,21 @@ export const ServicesSection = () => {
       className="py-20 lg:py-32 bg-gray-950 relative"
       style={{ marginTop: '-100px' }}
     >
-      {/* SVG notch at the top - filled completely with section color */}
+      {/* SVG notch at the top - sides filled, center transparent */}
       <svg
         className="absolute top-0 left-0 w-full h-[100px] pointer-events-none z-10"
         viewBox="0 0 1200 100"
         preserveAspectRatio="none"
         xmlns="http://www.w3.org/2000/svg"
       >
-        {/* Notch shape filled - points upward */}
+        {/* Left side - from left edge to notch */}
         <path
-          d="M0,0 L300,0 Q320,0 330,15 L380,75 Q400,95 420,95 L780,95 Q800,95 820,75 L870,15 Q880,0 900,0 L1200,0 L1200,100 L0,100 Z"
+          d="M0,0 L0,100 L300,100 Q320,100 330,85 L380,25 Q400,5 420,5 L420,0 Z"
+          className="fill-gray-950"
+        />
+        {/* Right side - from notch to right edge */}
+        <path
+          d="M780,0 L780,5 Q800,5 820,25 L870,85 Q880,100 900,100 L1200,100 L1200,0 Z"
           className="fill-gray-950"
         />
         {/* Accent line on notch edge */}
